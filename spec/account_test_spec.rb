381413d143c3d850717ce2c55c2cb4681969b27b
atejszerska <aax847@gmail.com>
2017-01-04 22:18:14 +0100

    Create account_test_spec.rb

---
+++ spec/account_test_spec.rb
@@ -0,0 +1,43 @@
+require 'simplecov'
+SimpleCov.start
+
+require 'account'
+
+RSpec.describe 'account.rb' do
+
+  before :each do
+    @account = Account.new :id, :name, :balance, :pin
+  end
+
+  describe "#new" do
+    it "should returns a account object" do
+      expect(@account).to be_an_instance_of(Account)
+    end
+  end
+
+  describe "id" do
+    it "should returns a correct id" do
+      expect(@account.id).to eq(:id)
+    end
+  end
+
+  describe "name" do
+    it "should returns a correct name" do
+      expect(@account.name).to eq(:name)
+    end
+  end
+
+  describe "balance" do
+    it "should returns a correct balance" do
+      expect(@account.balance).to eq(:balance)
+    end
+  end
+
+  describe "pin" do
+    it "should returns a correct pin" do
+      expect(@account.pin).to eq(:pin)
+    end
+  end
+
+
+end
