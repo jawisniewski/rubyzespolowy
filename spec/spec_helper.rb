8bf8a7c3563e6948fae105f2c6828e241e02b999
atejszerska <aax847@gmail.com>
2017-01-07 16:58:50 +0100

    Add files via upload

---
+++ spec/spec_helper.rb
@@ -0,0 +1,9 @@
+require 'rspec'
+require 'simplecov'
+
+SimpleCov.start
+
+require_relative '../lib/account'
+require_relative '../lib/bank'
+require_relative '../lib/menu'
+require_relative '../lib/person'
